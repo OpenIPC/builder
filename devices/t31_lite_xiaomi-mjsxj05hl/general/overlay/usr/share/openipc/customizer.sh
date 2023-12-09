2e3b1b1c1b686de8dbc36ec52d6ecac69517b822
cronyx <xcronyx@gmail.com>
2023-12-09 20:40:20 +0300

    add wansview q5 2k and xiaomi aw200

---
+++ devices/t31_lite_xiaomi-mjsxj05hl/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/t31_lite_xiaomi-mjsxj05hl-nor.tgz'
+#
+#
+# Set custom majestic settings
+#
+cli -s .system.webAdmin disabled
+cli -s .system.staticDir /var/www/majestic
+cli -s .isp.blkCnt 1
+#cli -s .nightMode.enabled true
+#cli -s .nightMode.irCutPin1 49
+#cli -s .nightMode.irCutPin2 50
+#cli -s .nightMode.backlightPin 60
+cli -s .video0.codec h264
+#
+#
+# Set wlan device and credentials if need
+#
+fw_setenv wlandev atbm603x-generic
+#fw_setenv wlanssid Router
+#fw_setenv wlanpass 12345678
+#
+#
+# Set osmem and rmem
+#
+fw_setenv osmem 39M
+fw_setenv rmem 25M@0x2700000
+#
+exit 0
