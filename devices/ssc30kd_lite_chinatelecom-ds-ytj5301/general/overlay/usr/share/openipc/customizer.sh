7a9838be163d2854fe87283c226d058d9dbf301f
Signor Pellegrino (from Citadel PC) <flyrouter@gmail.com>
2023-12-15 23:51:28 +0300

    Add chinatelecom-ds-ytj5301 device

---
+++ devices/ssc30kd_lite_chinatelecom-ds-ytj5301/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc335de_lite_chinatelecom-ds-ytj5301-nor.tgz'
+#
+#
+# Set custom majestic settings
+#
+cli -s .system.staticDir /var/www/majestic
+#cli -s .image.mirror true
+#cli -s .image.flip true
+#cli -s .nightMode.enabled true
+#cli -s .nightMode.irCutPin1 61
+#cli -s .nightMode.irCutPin2 79
+#cli -s .nightMode.backlightPin 4
+#cli -s .audio.speakerPin 0
+cli -s .video0.codec h264
+#
+#
+# Set wlan device and credentials if need
+#
+fw_setenv wlandev rtl8188fu-ssc30kq-chinatelecom-ds-ytj5301
+#fw_setenv wlanssid Router
+#fw_setenv wlanpass 12345678
+
+exit 0
