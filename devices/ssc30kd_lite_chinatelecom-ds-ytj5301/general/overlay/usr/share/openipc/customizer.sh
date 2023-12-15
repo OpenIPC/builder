b4f06188fef998db8e7052d9bdd53d8b4555b1aa
Signor Pellegrino (from Citadel PC) <flyrouter@gmail.com>
2023-12-16 00:23:24 +0300

    Fix customizer.sh for SSC30KQ

--- devices/ssc30kd_lite_chinatelecom-ds-ytj5301/general/overlay/usr/share/openipc/customizer.sh
+++ devices/ssc30kd_lite_chinatelecom-ds-ytj5301/general/overlay/usr/share/openipc/customizer.sh
@@ -5,7 +5,7 @@
 #
 # Set custom upgrade url
 #
-fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc30kq_lite_chinatelecom-ds-ytj5301-nor.tgz'
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc30kd_lite_chinatelecom-ds-ytj5301-nor.tgz'
 #
 #
 # Set custom majestic settings
