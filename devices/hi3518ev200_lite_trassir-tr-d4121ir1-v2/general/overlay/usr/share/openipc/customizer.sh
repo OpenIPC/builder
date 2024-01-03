46e7b1c43fa66a39a28af705ec2d3eaf407f2e9a
cronyx <xcronyx@gmail.com>
2024-01-03 22:10:03 +0300

    add TRASSIR TR-D4121IR1 v2

---
+++ devices/hi3518ev200_lite_trassir-tr-d4121ir1-v2/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/hi3518ev200_lite_trassir-tr-d4121ir1-v2-nor.tgz'
+#
+#
+# Set custom majestic settings
+#
+cli -s .system.staticDir /var/www/majestic
+cli -s .nightMode.enabled true
+#cli -s .nightMode.irSensorPin ??
+#cli -s .nightMode.irSensorPinInvert true
+#cli -s .nightMode.irCutPin1 ??
+#cli -s .nightMode.irCutPin2 ??
+#cli -s .nightMode.backlightPin ??
+cli -s .video0.codec h264
+cli -s .video0.fps 30
+cli -s .isp.slowShutter disabled
+#
+# front white led gpio - 0
+# reset gpio - ?
+#
+# Set wlan device and credentials if need
+#
+#fw_setenv wlandev mt7601u-generic
+#fw_setenv wlanssid Router
+#fw_setenv wlanpass 12345678
+
+exit 0
