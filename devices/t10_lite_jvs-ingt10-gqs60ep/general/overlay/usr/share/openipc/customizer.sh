c4ad85f584acfcc3a2cdc76d5e88e279c32cf3ee
john doe <47976944+johndi3@users.noreply.github.com>
2024-01-28 01:18:11 +0800

    Add JVS INGT10 GQS60EP T10A+OV9750 (#8)
    * Add JVS INGT10 GQS60EP T10A+OV9750
    
    * move ov9750 config and calibration file to firmware repo
    
    * move ov9750 config and calibration file to firmware repo

---
+++ devices/t10_lite_jvs-ingt10-gqs60ep/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/t10_lite_jvs-ingt10-gqs60ep-nor.tgz'
+#
+#
+# Set custom majestic settings
+#
+cli -s .nightMode.enabled true
+cli -s .nightMode.irSensorPin 19
+cli -s .nightMode.irCutPin1 25
+cli -s .nightMode.irCutPin2 26
+cli -s .video0.codec h264
+#
+#
+# Set wlan device and credentials if need
+#
+#fw_setenv wlandev rtl8188fu-generic
+#fw_setenv wlanssid Router
+#fw_setenv wlanpass 12345678
+#
+#
+# Set osmem and rmem
+#
+#fw_setenv osmem 39M
+#fw_setenv rmem 25M@0x2700000
+#
+exit 0
