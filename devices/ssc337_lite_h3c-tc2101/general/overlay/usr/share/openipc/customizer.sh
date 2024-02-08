1be8492cf95101d9f91aaa8937407c53113af907
john doe <47976944+johndi3@users.noreply.github.com>
2024-02-08 16:20:01 +0800

    add h3c tc2101 (#11)
    * add h3c tc2101
    
    * add speaker gpio pin for h3c tc2101
    
    * add qr scan package for h3c tc2101

---
+++ devices/ssc337_lite_h3c-tc2101/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc337_lite_h3c-tc2101-nor.tgz'
+#
+#
+# Set custom majestic settings
+#
+cli -s .nightMode.enabled true
+cli -s .nightMode.irCutPin1 79
+cli -s .nightMode.irCutPin2 78
+cli -s .nightMode.backlightPin 61
+cli -s .audio.speakerPin 15
+#
+#
+# Set wlan device and credentials if need
+#
+fw_setenv wlandev rtl8188fu-generic
+#fw_setenv wlanssid Router
+#fw_setenv wlanpass 12345678
+
+exit 0
