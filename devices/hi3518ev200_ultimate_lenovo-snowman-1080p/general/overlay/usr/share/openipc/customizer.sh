143f2ff324d1e7e77cc96a7792a0514db2c48590
githubtmp7 <tmp7@gradient.ru>
2024-02-11 14:31:39 +0000

    Delete undeeded option and set wlandev env variable

--- devices/hi3518ev200_ultimate_lenovo-snowman-1080p/general/overlay/usr/share/openipc/customizer.sh
+++ devices/hi3518ev200_ultimate_lenovo-snowman-1080p/general/overlay/usr/share/openipc/customizer.sh
@@ -7,7 +7,7 @@
 #
 fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/hi3518ev200_ultimate_lenovo-snowman-1080p-nor.tgz'
 fw_setenv sensor 'sc2035'
-fw_setenv wlandev 'rtl8188eus-lenovo-snowman'
+fw_setenv wlandev 'rtl8188eus-hi3518ev200-lenovo'
 #
 #
 # Set custom majestic settings
@@ -17,7 +17,6 @@ cli -s .nightMode.enabled true
 cli -s .nightMode.irCutPin1 33
 cli -s .nightMode.irCutSingleInvert true
 cli -s .nightMode.backlightPin 50
-cli -s .ips.sensorConfig /etc/sensors/sc2035_i2c_1080p.ini
 cli -s .audio.speakerPin 37
 
 #
