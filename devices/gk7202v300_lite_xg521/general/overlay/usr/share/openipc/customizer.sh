f7840ae1af12360067250cb9d9c7a6842fe576c4
johndi3 <gayhub@fastmail.com>
2024-02-25 17:00:55 +0800

    add xg521 gk7202v300+gc1054 with LTE modem

---
+++ devices/gk7202v300_lite_xg521/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7202v300_lite_xg521-nor.tgz'
+#
+# Set custom majestic settings
+#
+cli -s .nightMode.enabled true
+cli -s .nightMode.irCutPin1 15
+cli -s .nightMode.irCutPin2 14
+cli -s .nightMode.colorToGray false
+cli -s .nightMode.backlightPin 54
+cli -s .video0.codec h264
+cli -s .audio.speakerPin 4
+#
+# Set wlan device and credentials if need
+#
+fw_setenv wlandev ec800e-gk7202v300-xg521
+
+exit 0
