f93ad47960b350e803af903688f330fca1735884
Pun0110 <vm.tblet@gmail.com>
2024-03-20 16:24:24 +0300

    Some tweaks for gk7202v300_lite_cootli_camv0103

--- devices/gk7202v300_lite_cootli_camv0103/general/overlay/usr/share/openipc/customizer.sh
+++ devices/gk7202v300_lite_cootli_camv0103/general/overlay/usr/share/openipc/customizer.sh
@@ -9,11 +9,23 @@
 #
 fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7202v300_lite_cootli_camv0103-nor.tgz'
 #
-#
 # Set wlan device and credentials if need
 #
 fw_setenv wlandev ssv6x5x-generic
 #fw_setenv wlanssid Router
 #fw_setenv wlanpass 12345678
+#
+# Set custom majestic settings
+#
+cli -s .nightMode.enabled true
+cli -s .nightMode.irCutPin1 56
+cli -s .nightMode.irCutPin2 58
+cli -s .nightMode.backlightPin 16
+cli -s .nightMode.minThreshold 2000
+cli -s .nightMode.maxThreshold 17000
+cli -s .audio.speakerPin 51
+cli -s .audio.enabled true
+cli -s .audio.outputEnabled true
+cli -s .audio.speakerPin 51
 
 exit 0
