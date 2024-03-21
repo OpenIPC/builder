8ac422af486bbd1b7bbf0aa78f13573a890ff2e2
Pun0110 <vm.tblet@gmail.com>
2024-03-21 19:47:34 +0300

    Majestic settings & Startup scripts (device gk7202v300_lite_cootli_camv0103)
    - In customizer.sh: added pins for IRCUT, IRLED, Speaker
    - init.d -> S35pinworks: workaround about SD Card power and reinit
    - init.d -> S80pinworks: Unmuxing GPIO pins routed to motors connector
    
    Article related to this board https://github.com/OpenIPC/wiki/blob/master/en/device-IPC-RM1-BLK7202V3-M43A-WIFI.md

--- devices/gk7202v300_lite_cootli_camv0103/general/overlay/usr/share/openipc/customizer.sh
+++ devices/gk7202v300_lite_cootli_camv0103/general/overlay/usr/share/openipc/customizer.sh
@@ -15,5 +15,17 @@ fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/g
 fw_setenv wlandev ssv6x5x-generic
 #fw_setenv wlanssid Router
 #fw_setenv wlanpass 12345678
+#
+# Set majestic settings
+#
+cli -s .nightMode.enabled true
+cli -s .nightMode.irCutPin1 56
+cli -s .nightMode.irCutPin2 58
+cli -s .nightMode.backlightPin 16
+cli -s .nightMode.minThreshold 2000
+cli -s .nightMode.maxThreshold 17000
+cli -s .audio.enabled true
+cli -s .audio.outputEnabled true
+cli -s .audio.speakerPin 51
 
 exit 0
