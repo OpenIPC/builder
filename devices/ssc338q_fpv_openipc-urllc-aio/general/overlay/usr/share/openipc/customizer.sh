cba9d03d312e125f53bd98a61c44ae5e7ddc23cc
Viktor <35473052+viktorxda@users.noreply.github.com>
2024-04-29 05:18:16 +0200

    Update Update ssc338q_fpv_openipc

--- devices/ssc338q_fpv_openipc-urllc-aio/general/overlay/usr/share/openipc/customizer.sh
+++ devices/ssc338q_fpv_openipc-urllc-aio/general/overlay/usr/share/openipc/customizer.sh
@@ -3,4 +3,7 @@
 # Set custom upgrade url
 fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc338q_fpv_openipc-urllc-aio-nor.tgz'
 
+# Set custom majestic settings
+cli -s .video0.codec h265
+
 exit 0
