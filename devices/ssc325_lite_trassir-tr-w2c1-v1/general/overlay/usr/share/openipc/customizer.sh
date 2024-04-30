ee94e70a305a9ccf48eb74393805df61e67d3ede
cronyx <xcronyx@gmail.com>
2024-04-30 12:53:23 +0300

    gpios for Trassir TR-W2C1 v1

--- devices/ssc325_lite_trassir-tr-w2c1-v1/general/overlay/usr/share/openipc/customizer.sh
+++ devices/ssc325_lite_trassir-tr-w2c1-v1/general/overlay/usr/share/openipc/customizer.sh
@@ -10,10 +10,14 @@ fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/s
 #
 # Set custom majestic settings
 #
-#cli -s .nightMode.irCutPin1 78
-#cli -s .nightMode.irCutPin2 79
-#cli -s .nightMode.backlightPin 52
-#cli -s .nightMode.colorToGray true
+cli -s .nightMode.irCutPin1 79
+cli -s .nightMode.irCutPin2 78
+cli -s .nightMode.backlightPin 14
+cli -s .nightMode.colorToGray true
+cli -s .image.mirror true
+cli -s .image.flip true
+cli -s .audio.speakerPin 15
+cli -s .audio.outputVolume 80
 cli -s .video0.codec h264
 #
 #
