324d5dc8a793450593559559361677041219db4c
Signor Pellegrino (from Citadel PC) <i.zalatov@openipc.org>
2025-01-18 00:58:32 +0300

    Add tiandy-tc-c32qn profile

---
+++ devices/gk7205v210_lite_tiandy-tc-c32qn/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7205v210_lite_tiandy-tc-c32qn-nor.tgz'
+#
+#
+# Set custom majestic settings
+#
+cli -s .video0.codec h264
+cli -s .audio.enabled true
+cli -s .audio.volume 50
+cli -s .nightMode.irCutPin1 52
+cli -s .nightMode.irCutPin2 53
+cli -s .nightMode.backlightPin 4
+cli -s .nightMode.lightMonitor false
+cli -s .nightMode.monitorDelay 3
+cli -s .nightMode.minThreshold 2000
+cli -s .nightMode.maxThreshold 14000
+#
+#
+# Set wlan device and credentials if need
+#
+#fw_setenv wlandev unknown
+#fw_setenv wlanssid Router
+#fw_setenv wlanpass 12345678
+#
+adduser viewer -s /bin/false -D -H
+echo viewer:123456 | chpasswd
+#
+
+exit 0
