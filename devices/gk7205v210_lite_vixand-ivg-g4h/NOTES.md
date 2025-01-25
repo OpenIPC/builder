a3f1ab94f444fae84dbe0237463180a7f125de32
Signor Pellegrino (from Citadel PC) <i.zalatov@openipc.org>
2025-01-26 00:05:57 +0300

    Add profile for XM G4H

---
+++ devices/gk7205v210_lite_vixand-ivg-g4h/NOTES.md
@@ -0,0 +1,13 @@
+![OpenIPC logo][logo]
+
+## OpenIPC Builder
+_Experimental system for building OpenIPC firmware for known devices_
+
+
+### Release notes for the device
+
+- This device profile contains the experimental binary component of the sensor module from the OpenIPC sensors project.
+- Currently, the sensor module works, but we are working on improvements.
+
+
+[logo]: https://openipc.org/assets/openipc-logo-black.svg
---
+++ devices/gk7205v210_lite_vixand-ivg-g4h/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7205v200_lite_vixand-ivg-g4h-nor.tgz'
+#
+#
+# Set custom majestic settings
+#
+cli -s .nightMode.irCutPin1 8
+cli -s .nightMode.irCutPin2 9
+cli -s .nightMode.backlightPin 14
+cli -s .nightMode.lightMonitor true
+cli -s .nightMode.minThreshold 2000
+cli -s .nightMode.maxThreshold 14000
+#cli -s .audio.speakerPin 55
+cli -s .video0.codec h264
+#
+#
+# Set wlan device and credentials if need
+#
+#fw_setenv wlandev unknown
+#fw_setenv wlanssid Router
+#fw_setenv wlanpass 12345678
+#
+adduser viewer -s /bin/false -D -H
+echo viewer:123456 | chpasswd
+#
+
+exit 0
+
