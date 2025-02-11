ca332c8aff585b509bfa935b5f802d85c41f889c
Petru Soroaga <petrusoroaga@yahoo.com>
2025-02-11 21:18:48 +0200

    Updated builder steps and configs for RubyFPV (#42)

--- devices/ssc338q_rubyfpv_thinker/general/overlay/usr/share/openipc/customizer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Set custom upgrade url
-fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc338q_rubyfpv_thinker-nor.tgz'
-
-exit 0
---
+++ devices/ssc338q_rubyfpv_thinker_internal_wifi/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Set custom upgrade url
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc338q_rubyfpv_thinker_internal_wifi-nor.tgz'
+
+exit 0
