fccea740396910aa229a2c6f67b3aec5ffc150e3
Signor Pellegrino (from Citadel PC) <i.zalatov@openipc.org>
2025-03-03 11:25:44 +0300

    Update ssc337_lite_tiandy-tc-c321n-v2 profile

--- devices/ssc337_lite_tiandy-tc-c321n-v2/general/overlay/usr/share/openipc/customizer.sh
+++ devices/ssc337_lite_tiandy-tc-c321n-v2/general/overlay/usr/share/openipc/customizer.sh
@@ -13,6 +13,8 @@ fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/s
 cli -s .nightMode.irCutPin1 78
 cli -s .nightMode.irCutPin2 79
 cli -s .nightMode.backlightPin 37
+cli -s .audio.enabled true
+cli -s .audio.volume 50
 #
 #
 # Set wlan device and credentials if need
