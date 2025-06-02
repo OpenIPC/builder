9bbe14e290349602118c86598db376ca5eb6e697
viktorxda <35473052+viktorxda@users.noreply.github.com>
2025-06-02 14:35:22 +0200

    Update ssc338q_apfpv_defconfig

--- devices/ssc338q_apfpv/general/overlay/usr/share/openipc/customizer.sh
+++ devices/ssc338q_apfpv/general/overlay/usr/share/openipc/customizer.sh
@@ -12,8 +12,11 @@ fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/o
 #
 cli -s .isp.exposure 10
 cli -s .video0.fps 90
-cli -s .video0.bitrate 8192
+cli -s .video0.bitrate 12000
 cli -s .video0.codec h265
 cli -s .video0.rcMode cbr
+cli -s .audio.codec pcm
 cli -s .outgoing.enabled true
 cli -s .outgoing.server udp://192.168.0.10:5600
+cli -s .records.split 5
+cli -s .records.notime true
