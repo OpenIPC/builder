367458121964a702af858b0a0907cfbf10d08282
viktorxda <35473052+viktorxda@users.noreply.github.com>
2025-06-07 23:22:11 +0200

    Update ssc338q_apfpv_defconfig

--- devices/ssc338q_apfpv/general/overlay/usr/share/openipc/customizer.sh
+++ devices/ssc338q_apfpv/general/overlay/usr/share/openipc/customizer.sh
@@ -10,11 +10,11 @@ fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/o
 #
 # Set custom majestic settings
 #
-cli -s .isp.exposure 10
-cli -s .video0.fps 90
-cli -s .video0.bitrate 12000
+cli -s .isp.exposure 15
+cli -s .video0.fps 60
+cli -s .video0.bitrate 10000
 cli -s .video0.codec h265
-cli -s .video0.rcMode cbr
+cli -s .video0.rcMode avbr
 cli -s .audio.codec pcm
 cli -s .outgoing.enabled true
 cli -s .outgoing.server udp://192.168.0.10:5600
