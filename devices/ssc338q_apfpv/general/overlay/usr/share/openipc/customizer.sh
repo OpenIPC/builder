3a64a559f92610ba32d959cc721b782fbb5df1e6
viktorxda <35473052+viktorxda@users.noreply.github.com>
2025-07-08 20:02:51 +0200

    Update apfpv

--- devices/ssc338q_apfpv/general/overlay/usr/share/openipc/customizer.sh
+++ devices/ssc338q_apfpv/general/overlay/usr/share/openipc/customizer.sh
@@ -15,8 +15,7 @@ cli -s .video0.fps 60
 cli -s .video0.bitrate 10000
 cli -s .video0.codec h265
 cli -s .video0.rcMode avbr
-cli -s .audio.codec pcm
 cli -s .outgoing.enabled true
-cli -s .outgoing.server udp://192.168.0.10:5600
+cli -s .outgoing.server udp://224.0.0.1:5600
 cli -s .records.split 5
 cli -s .records.notime true
