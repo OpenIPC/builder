e0be0983287eeb93b03c6e62998d57889989a137
Puzzak01 <puzzak@my.com>
2025-08-27 10:11:06 +0300

    Create customizer.sh

---
+++ devices/ssc335de_lite_dahua-hfw1230sp-v2/general/usr/share/openipc/customizer.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc335de_lite_dahua-hfw1230sp-v2-nor.tgz'
+#
+#
+# Set custom majestic settings
+#
+cli -s .video0.codec h264
+cli -s .image.mirror false
+cli -s .image.flip false
+cli -s .nightMode.irCutPin1 79
+cli -s .nightMode.irCutPin2 78
+cli -s .nightMode.backlightPin 52
+cli -s .nightMode.colorToGray true
+#
+
+exit 0
