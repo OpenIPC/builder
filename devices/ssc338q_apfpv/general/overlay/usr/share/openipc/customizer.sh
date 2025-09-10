3efab67c2d3ef7bd814359b3b23faeb7957dff4e
Signor Pellegrino <68112357+flyrouter@users.noreply.github.com>
2025-09-10 19:32:26 +0300

    Update customizer.sh

--- devices/ssc338q_apfpv/general/overlay/usr/share/openipc/customizer.sh
+++ devices/ssc338q_apfpv/general/overlay/usr/share/openipc/customizer.sh
@@ -14,7 +14,7 @@ cli -s .isp.exposure 10
 cli -s .video0.fps 90
 cli -s .video0.bitrate 12000
 cli -s .video0.codec h265
-cli -s .video0.rcMode avbr
+cli -s .video0.rcMode cbr
 cli -s .audio.codec pcm
 cli -s .outgoing.enabled true
 cli -s .outgoing.server udp://192.168.0.10:5600
