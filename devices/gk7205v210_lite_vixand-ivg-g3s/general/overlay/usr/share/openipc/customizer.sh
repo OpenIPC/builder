7c87592428cbc16f2f07e8014df19b5f89f8eeda
Signor Pellegrino (from Citadel PC) <i.zalatov@openipc.org>
2025-09-25 20:42:50 +0300

    Update Vixand configs

--- devices/gk7205v210_lite_vixand-ivg-g3s/general/overlay/usr/share/openipc/customizer.sh
+++ devices/gk7205v210_lite_vixand-ivg-g3s/general/overlay/usr/share/openipc/customizer.sh
@@ -5,20 +5,19 @@
 #
 # Set custom upgrade url
 #
-fw_setenv soc gk7205v210
 fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7205v210_lite_vixand-ivg-g3s-nor.tgz'
 #
 #
 # Set custom majestic settings
 #
-cli -s .nightMode.irCutPin1 8
-cli -s .nightMode.irCutPin2 9
+cli -s .video0.codec h264
+#cli -s .audio.speakerPin 55
+cli -s .nightMode.irCutPin1 9
+cli -s .nightMode.irCutPin2 8
 cli -s .nightMode.backlightPin 16
 #cli -s .nightMode.lightMonitor true
 cli -s .nightMode.minThreshold 2000
 cli -s .nightMode.maxThreshold 14000
-#cli -s .audio.speakerPin 55
-cli -s .video0.codec h264
 #
 #
 # Set wlan device and credentials if need
@@ -26,6 +25,8 @@ cli -s .video0.codec h264
 #fw_setenv wlandev unknown
 #fw_setenv wlanssid Router
 #fw_setenv wlanpass 12345678
+#
+fw_setenv soc gk7205v210
 #
 adduser viewer -s /bin/false -D -H
 echo viewer:123456 | chpasswd
--- devices/gk7205v210_lite_vixand-ivg-g4f-a/general/overlay/usr/share/openipc/customizer.sh
+++ devices/gk7205v210_lite_vixand-ivg-g4f-a/general/overlay/usr/share/openipc/customizer.sh
@@ -5,19 +5,19 @@
 #
 # Set custom upgrade url
 #
-fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7205v200_lite_vixand-ivg-g4f-a-nor.tgz'
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7205v210_lite_vixand-ivg-g4f-a-nor.tgz'
 #
 #
 # Set custom majestic settings
 #
-cli -s .nightMode.irCutPin1 8
-cli -s .nightMode.irCutPin2 9
-cli -s .nightMode.backlightPin 14
+cli -s .video0.codec h264
+#cli -s .audio.speakerPin 55
+cli -s .nightMode.irCutPin1 9
+cli -s .nightMode.irCutPin2 8
+cli -s .nightMode.backlightPin 16
 #cli -s .nightMode.lightMonitor true
 cli -s .nightMode.minThreshold 2000
 cli -s .nightMode.maxThreshold 14000
-#cli -s .audio.speakerPin 55
-cli -s .video0.codec h264
 #
 #
 # Set wlan device and credentials if need
@@ -25,6 +25,8 @@ cli -s .video0.codec h264
 #fw_setenv wlandev unknown
 #fw_setenv wlanssid Router
 #fw_setenv wlanpass 12345678
+#
+fw_setenv soc gk7205v210
 #
 adduser viewer -s /bin/false -D -H
 echo viewer:123456 | chpasswd
