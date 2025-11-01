652edce4a264327fd35a5db5cbad696a6b853793
Signor Pellegrino (from Citadel PC) <i.zalatov@openipc.org>
2025-11-01 17:04:41 +0300

    Update Vixand devices

--- devices/gk7205v300_lite_vixand-ivg-g6s/general/overlay/usr/share/openipc/customizer.sh
+++ devices/gk7205v300_lite_vixand-ivg-g6s/general/overlay/usr/share/openipc/customizer.sh
@@ -8,21 +8,56 @@
 fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7205v300_lite_vixand-ivg-g6s-nor.tgz'
 #
 #
+# Set custom majestic settings
+#
+cli -s .isp.blkCnt 5
+cli -s .isp.drc 200
+cli -s .video0.codec h264
+cli -s .video0.bitrate 7168
+cli -s .video0.rcMode vbr
+cli -s .video0.fps 25
+cli -s .video0.size 2592x1520
+cli -s .video1.enabled true
+cli -s .video1.fps 15
+cli -s .video1.size 704x396
+cli -s .jpeg.size 1920x1080
+cli -s .jpeg.qfactor 80
+cli -s .osd.enabled true
+#cli -s .osd.posX 10
+#cli -s .osd.posY 10
+cli -s .osd.template "VIXAND | %F %T %Z"
+cli -s .audio.enabled true
+cli -s .audio.volume 50
+cli -s .audio.srate 32000
+cli -s .audio.codec aac
+#cli -s .audio.speakerPin 55
+cli -s .nightMode.colorToGray false
+#cli -s .nightMode.irCutPin1 11
+#cli -s .nightMode.irCutPin2 10
+cli -s .nightMode.backlightPin 4
+cli -s .nightMode.lightMonitor true
+cli -s .nightMode.minThreshold 1800
+cli -s .nightMode.maxThreshold 32000
+cli -s .motionDetect.enabled true
+cli -s .motionDetect.visualize false
+cli -s .motionDetect.debug true
+cli -s .onvif.enabled true
+cli -s .netip.enabled true
+cli -s .netip.user viewer
+cli -s .netip.password tlJwpbo6
+cli -s .netip.ignoreSetTime true
+#
 # Set wlan device and credentials if need
 #
-# fw_setenv wlandev atbm603x-gk7205v300-xm-g6s
+#fw_setenv wlandev atbm603x-gk7205v300-xm-g6s
 #fw_setenv wlanssid Router
 #fw_setenv wlanpass 12345678
 #
-# Set majestic settings
+fw_setenv totalmem 128M
+fw_setenv osmem 64M
+set_allocator cma
 #
-cli -s .nightMode.irCutPin1 11
-cli -s .nightMode.irCutPin2 10
-#cli -s .nightMode.backlightPin 14
-#cli -s .nightMode.lightMonitor true
-#cli -s .nightMode.minThreshold 2000
-#cli -s .nightMode.maxThreshold 14000
-#cli -s .audio.speakerPin 55
-cli -s .video0.codec h264
+(sleep 3 ; reboot -f) &
+#
 
 exit 0
