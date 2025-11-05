eadd4c1faaf31785bc594e36332506ca4af70228
harahash.v <elvoblin@mail.ru>
2025-11-05 19:10:26 +0300

    wip: gk7202v300_lite_ipg-g3-wr

--- devices/gk7202v300_lite_ipg-g3-wr/general/overlay/usr/share/openipc/customizer.sh
+++ devices/gk7202v300_lite_ipg-g3-wr/general/overlay/usr/share/openipc/customizer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7202v300_lite_ipg-g3-wr-nor.tgz'
+
 fw_setenv wlandev atbm603x-generic-usb
 fw_setenv sensor jxh63_i2c_720p
 
