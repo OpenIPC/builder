d7a782d1375a6e95fef0c5141482d68c3191775d
Signor Pellegrino (from Citadel PC) <i.zalatov@openipc.org>
2026-04-11 18:50:20 +0300

    Move ssc377qe_fpv_generic profile to real ssc377qe_fpv_ccdcam-im50q01-tipoman

---
+++ devices/ssc377qe_fpv_ccdcam-im50q01-tipoman/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc377qe_fpv_ccdcam-im50q01-tipoman-nor.tgz'
+#
+
+exit 0
