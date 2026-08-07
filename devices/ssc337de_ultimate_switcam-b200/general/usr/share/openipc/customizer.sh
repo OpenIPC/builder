acf00f5800b2d4666c25d8af1ef364941173a177
Puzzak01 <puzzak@my.com>
2026-08-07 21:41:46 +0300

    Create customizer.sh

---
+++ devices/ssc337de_ultimate_switcam-b200/general/usr/share/openipc/customizer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc337de_ultimate_switcam-b200-nand.tgz'
+#
+#
+# Set custom majestic settings
+#
+cli -s .video0.codec h264
+cli -s .image.mirror false
+cli -s .image.flip false
+cli -s .nightMode.irCutPin1 13
+cli -s .nightMode.irCutPin2 12
+cli -s .nightMode.backlightPin 52
+cli -s .nightMode.colorToGray true
+cli -s .audio.enabled true
+cli -s .audio.codec aac
+cli -s .audio.srate 48000
+cli -s .audio.inputChannel 1
+#
+
+exit 0
